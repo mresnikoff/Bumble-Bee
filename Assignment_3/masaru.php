3ae4c72310d4f6b04d1394463da135bc5284aa44
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-02-28 15:30:04 -0500

    Update masaru.php

--- Assignment_3/masaru.php
+++ Assignment_3/masaru.php
@@ -14,6 +14,10 @@
 			<li> Born in Ibaraki, Japan (city next to Tokyo), and spent first 7 years of my life there. Then, I moved to Shanghai, China </li>
 			<li> I had an internship at CITIC Securities Co., Ltd, Investment Banking Department </li>
 			<li> Interesting fact about me is that I am half Japanese and half Chinese so I can speak three languages! </li> 
+			<li> I am majoring in financial economics and statistics and minoring in computer science.</li>
+			<li> I have taken the following courses: Financial Accounting; Managerial Accounting; Web page designing, The science of programming; computational introduction to statistics; microeconomics; macroeconomics; econometrics; economics of globalization; industrial organization; financial management; investments; calculus; multidimensional calculus; probability; linear algebra with differential equation; financial mathematics; Spanish; math statistics. </li>
+			<li> At my internship, I summarized industry reports associated with our client company to compare various business operations, wrote industry reports to our client company and suggested key business plans and also provided accurate information of the market. </li>
+			<li> In addition, I valued other companies to identify the pros and cons in order to adopt new regulations, and attended several meetings about the prospectus of the client company and resolve problems encountered in listing. Also contributed in preparing pitch book and teasers for other projects. Gained understanding of IPO procedures, detailed information about specific industries, and difficulties that companies face during listing period. </li>
 		</ul>
 
 			

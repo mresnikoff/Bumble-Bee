243f87cec3d4f92b4584344c80213ce5134b7922
AlbertNissim <35936842+AlbertNissim@users.noreply.github.com>
2018-02-28 12:44:15 -0500

    Update masaru.php

--- Assignment_3/masaru.php
+++ Assignment_3/masaru.php
@@ -11,15 +11,15 @@
 <img src="images/masaru.png" alt="Fed">
 		
 		
-		<p>Born in Ibaraki, Japan (city next to Tokyo), and spent first 7 years of my life there. Then, I moved to Shanghai, China. Interesting fact about me is that I am half Japanese and half Chinese so I can speak three languages!. I play tennis for Shanghai team and UofR and I love to play sports. I have been the top player on the U of R tennis team since I arrived as a freshman, and was inspired to start playing when I was very young by the anime series, "Prince of Tennis". I have now become the captain of the team and look to lead them to a great season!
+		<p>
+		<ul>
+			<li> Born in Ibaraki, Japan (city next to Tokyo), and spent first 7 years of my life there. Then, I moved to Shanghai, China </li>
+			<li> I had an internship at CITIC Securities Co., Ltd, Investment Banking Department </li>
+			<li> Interesting fact about me is that I am half Japanese and half Chinese so I can speak three languages! </li> 
+		</ul>
 
-
-I had an internship at CITIC Securities Co., Ltd, Investment Banking Department.During the internship, worked for the Hong Kong IPO project. There I summarized industry reports associated with our client company to compare various business operations, wrote industry reports to our client company and suggested key business plans and also provided accurate information of the market. In addition, valued other companies to identify the pros and cons in order to adopt new regulations, and attended several meetings about the prospectus of the client company and resolve problems encountered in listing. Also contributed in preparing pitch book and teasers for other projects. Gained understanding of IPO procedures, detailed information about specific industries, and difficulties that companies face during listing period.
-		My education consists of majoring in financial economics and statistics and minoring in computer science.
 			
 
-			As a junior I am taking a lot of classes while back at school and was the social chair for my fraternity, Theta Chi, as well. I started my next internship only three days after getting back from school. In those three days I also got all of my wisdom teeth removed. The company I worked for last summer was is called Group 8A. It is a great place where I learned so many new skills and met some really great people. The office is in Chelsea Manhattan with a breathtaking view of both the Hudson River, Hudson Yards, and downtown NYC. The building the office is located in used to be a train station and there is even an elevator inside the building that can take large trucks up and down to each floor. There was a taco truck right outside the front doors of my office all summer called Takumi Taco. They had some really great tacos. My responsibilities were different depending on the day, but some of my biggest projects included setting up an analytics software for the company that would track how much money was being spent on each client and also tracking how those marketing dollars were creating revenue for each client.
-			Out of all my internships, working at group 8A was definitely my favorite. Outside of work I like to play sports, hang out with friends, and am constantly finding new music to listen to and play at parties. I like exploring different places to eat in Rochester as well. I have had multiple positons within my fraternity as well including Rush Chair, Social Chair, and alumni Liaison. I lived in the fraternity house during my sophomore and junior year at school and as a senior I am living off campus with some of my friends.
 
 
 
@@ -28,8 +28,18 @@ I had an internship at CITIC Securities Co., Ltd, Investment Banking Department.
 </div>
 	<aside>
 
-		<h2>More about Masaru</h2>
-		<p>The courses I've taken consist of Financial Accounting; Managerial Accounting; Web page designing, The science of programming; computational introduction to statistics; microeconomics; macroeconomics; econometrics; economics of globalization; industrial organization; financial management; investments; calculus; multidimensional calculus; probability; linear algebra with differential equation; financial mathematics; Spanish; math statistics.</p>
+		<h2> Tennis Highlights </h2>
+		<p> 
+		<ul> 
+			<li> I play tennis for Shanghai team and U of R </li> 
+			<li> I Was inspired to start playing when I was very young by the anime series, "Prince of Tennis" </li> 
+			<li> I have now become the captain of the team and look to lead them to a great season! </li>
+		</ul>
+
+			
+
+
+		</p>
 
 		
 	</aside>

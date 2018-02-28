5e72c64a21d9d1675422f73dd59d638adcb2280a
oostriker15 <35782802+oostriker15@users.noreply.github.com>
2018-02-28 15:26:23 -0500

    Update alex.php

--- Assignment_3/alex.php
+++ Assignment_3/alex.php
@@ -14,6 +14,10 @@
                 	<li> At the U of R Alex found his true passion – computer science </li>
                 	<li> I’m originally from Iowa and my ethnic heritage consists of quarter Indian, quarter German, and half Russian </li>
                 	<li> I have visited many countries. I have visited, as of today, Canada, Russia, England, Italy, Germany, Netherlands, Switzerland, France, Dominican Republic, Puerto Rico, Mexico, Denmark, Sweden, Norway and Iceland </li>
+			<li> Before applying to colleges, my father and I went on a road trip starting at the University of Rochester and ending at MIT. Along the way, we visited Hobart & William Smith Colleges, RPI, Clark University, Brandeis University, Bates College, and Bowdoin College before settling in central Boston. I enjoyed RPI, Rochester, Bowdoin and MIT the most out of that trip.</li>
+                	<li> The reason I chose to visit these colleges is that their admissions policy included optional submission of SAT and or ACT, as my test-taking skills were poor and was reflected on my SAT scores. </li>
+                	<li> I have taken a variety of courses at the university. I now am an Economics major with a minor in Audio & Music Engineering in my junior year. </li>
+			<li> Outside of academics, I am a freelance audio engineer and created BoredProductions.ml to display my work. I included a few words about why audio engineering is necessary, just so potential clients are convinced.</li>
                 </ul>
 
 			
